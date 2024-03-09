5b2e062f5614b18a3a6ce4d730fee627b38bcfeb
Jennie Andersson <jennie.andersson@vinnovera.se>
2024-03-09 12:12:48 +0100

    fix: remove intro owl

--- app/[locale]/components/OwlTips/index.tsx
+++ app/[locale]/components/OwlTips/index.tsx
@@ -2,7 +2,6 @@ import { AnimatePresence, motion } from 'framer-motion'
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 import {
-  getFirstTimePlaying,
   getShownEnableCardTip,
   getShownTokenTip,
   setShownEnableCardTip,
@@ -19,9 +18,7 @@ type Props = {
 }
 
 export const OwlTips = React.memo(({ showOwl, setShowOwl }: Props) => {
-  const [firstTimer, setFirstTimer] = useState<boolean | undefined>(undefined)
   const [hasShownTokenTip, setHasShownTokenTip] = useState(true)
-  const [hasShownIntroOwl, setHasShownIntroOwl] = useState(false)
   const [hasShownEnableCardTip, setHasShownEnableCardTip] = useState(false)
 
   function onHideTokenTip() {
@@ -35,30 +32,12 @@ export const OwlTips = React.memo(({ showOwl, setShowOwl }: Props) => {
       const shownEnableCardTip = await getShownEnableCardTip()
       setHasShownEnableCardTip(shownEnableCardTip)
       setHasShownTokenTip(shownTokenTip)
-      const firstTimePlaying = await getFirstTimePlaying()
-      setFirstTimer(firstTimePlaying)
     })()
   }, [])
 
   return (
     <div aria-live='polite'>
       <AnimatePresence>
-        {showOwl === OWLS.INTRO && !hasShownIntroOwl && firstTimer && (
-          <motion.div
-            className={styles.introOwlWrapper}
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            exit={{ scale: 0.5, opacity: 0 }}
-          >
-            <OwlDialogue
-              heading='Owl.intro.heading'
-              body='Owl.intro.body'
-              setShowSpecificOwl={setShowOwl}
-              setHasShownOwlTip={setHasShownIntroOwl}
-            />
-          </motion.div>
-        )}
-
         {!hasShownEnableCardTip && showOwl === OWLS.ENABLE_CARD && (
           <motion.div
             className={styles.enableOwlWrapper}
